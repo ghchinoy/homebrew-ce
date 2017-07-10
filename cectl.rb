625d2c02b490da01599800023b3bbc3ebda17da1
G. Hussain Chinoy <ghchinoy@gmail.com>
2017-07-10 10:36:08 -0600

    correct shasum

--- cectl.rb
+++ cectl.rb
@@ -2,7 +2,7 @@ class Cectl < Formula
   desc "Cloud Elements Platform CLI"
   homepage "https://github.com/ghchinoy/cectl"
   url "https://github.com/ghchinoy/cectl/releases/download/v0.2.0/cectl-0.2.0.tar.gz"
-  sha256 "56d9666e24f9c59049f7ed9952ea63ede8a948f67bf27c80057bc83ae8b13107"
+  sha256 "76e00c70e171afb3cb67d016f74c8da04219b70eda41ee8baae33c31b8e9c49d"
 
   bottle :unneeded
 
