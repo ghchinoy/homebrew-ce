f7cc359603063bc10d9872925586fb444cacb4cd
G. Hussain Chinoy <ghchinoy@gmail.com>
2018-11-18 10:30:20 -0700

    v0.17.4.2

--- cectl.rb
+++ cectl.rb
@@ -1,8 +1,8 @@
 class Cectl < Formula
   desc "Cloud Elements Platform CLI"
   homepage "https://github.com/ghchinoy/cectl"
-  url "https://github.com/ghchinoy/cectl/releases/download/v0.17.4/cectl-0.17.4.tar.gz"
-  sha256 "ccdbd00dd52be0c13a2d0e28ae352cba5b0ad22ff5d017f0deea7989cc7accdd"
+  url "https://github.com/ghchinoy/cectl/releases/download/v0.17.4.2/cectl-0.17.4.2.tar.gz"
+  sha256 "8dcb0bafffedae86d1bba5ccaa4075d7809d1f91c74a008055150e9f4b6aea8c"
 
   bottle :unneeded
 
